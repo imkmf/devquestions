da997b92e604af775f3f0ffd073f499db7f74dd3
Kristian Freeman <kristian@kristianfreeman.com>
2018-04-08 15:56:41 -0700

    Use full path for cover image

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,9 @@ const IndexPage = () => {
   const theme = loadTheme(data.question.text)
   const cssPath = `/static/css/${theme}.css?v=${data.meta.assetVersion}`
   const oneSignalPath = `/static/js/onesignal.js?v=${data.meta.assetVersion}`
-  const imagePath = `/static/img/cover.png?v=${data.meta.assetVersion}`
+  const coverPath = `https://devquestions.co/static/img/cover.png?v=${
+    data.meta.assetVersion
+  }`
 
   return (
     <article className="helvetica">
@@ -42,14 +44,14 @@ const IndexPage = () => {
         <meta property="og:description" content={data.meta.text} />
         <meta property="og:url" content={'https://devquestions.co'} />
         <meta property="og:site_name" content={'devquestions.co'} />
-        <meta property="og:image" content={withPrefix(imagePath)} />
-        <meta property="og:image:secure_url" content={withPrefix(imagePath)} />
+        <meta property="og:image" content={coverPath} />
+        <meta property="og:image:secure_url" content={coverPath} />
         <meta name="twitter:card" content="summary" />
 
         <meta name="twitter:description" content={data.meta.text} />
         <meta name="twitter:title" content={title} />
         <meta name="twitter:site" content={`@byteconf`} />
-        <meta name="twitter:image" content={withPrefix(imagePath)} />
+        <meta name="twitter:image" content={coverPath} />
       </Helmet>
       <header className="vh-100 base00-bg dt w-100">
         <div className="dtc v-mid cover ph3 ph4-m ph5-l">
