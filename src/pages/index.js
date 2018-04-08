e292e13536f210afe25e55a4d28766cf4fc15e48
Kristian Freeman <kristian@kristianfreeman.com>
2018-04-08 15:53:37 -0700

    add meta tags

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,14 +9,16 @@ import favicon from './favicon.ico'
 import loadTheme from '../styles/loadTheme'
 
 const IndexPage = () => {
+  const title = [data.question.text, 'devquestions'].join(' - ')
   const theme = loadTheme(data.question.text)
   const cssPath = `/static/css/${theme}.css?v=${data.meta.assetVersion}`
   const oneSignalPath = `/static/js/onesignal.js?v=${data.meta.assetVersion}`
+  const imagePath = `/static/img/cover.png?v=${data.meta.assetVersion}`
 
   return (
     <article className="helvetica">
       <Helmet
-        title={[data.question.text, 'devquestions'].join(' - ')}
+        title={title}
         meta={[{ name: 'description', content: data.meta.text }]}
       >
         <link rel="manifest" href={withPrefix('/manifest.json')} />
@@ -34,6 +36,20 @@ const IndexPage = () => {
           rel="stylesheet"
         />
         <link async href={withPrefix(cssPath)} rel="stylesheet" />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={data.meta.text} />
+        <meta property="og:url" content={'https://devquestions.co'} />
+        <meta property="og:site_name" content={'devquestions.co'} />
+        <meta property="og:image" content={withPrefix(imagePath)} />
+        <meta property="og:image:secure_url" content={withPrefix(imagePath)} />
+        <meta name="twitter:card" content="summary" />
+
+        <meta name="twitter:description" content={data.meta.text} />
+        <meta name="twitter:title" content={title} />
+        <meta name="twitter:site" content={`@byteconf`} />
+        <meta name="twitter:image" content={withPrefix(imagePath)} />
       </Helmet>
       <header className="vh-100 base00-bg dt w-100">
         <div className="dtc v-mid cover ph3 ph4-m ph5-l">
