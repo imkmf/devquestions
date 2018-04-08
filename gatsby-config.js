1cae9eccf0beeae138eace954570e5b6118bd4bb
Kristian Freeman <kristian@kristianfreeman.com>
2018-04-08 14:09:14 -0700

    Add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,14 @@
 module.exports = {
   pathPrefix: `/devquestions`,
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: 'UA-107917910-7',
+      },
+    },
+  ],
   siteMetadata: {
     title: 'devquestions.co',
   },
