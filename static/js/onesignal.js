c596159404ec517317f984bee39c261a4d5d85ef
Kristian Freeman <kristian@kristianfreeman.com>
2018-04-08 15:19:50 -0700

    Updates

--- static/js/onesignal.js
+++ static/js/onesignal.js
@@ -1,6 +1,9 @@
-  var OneSignal = window.OneSignal || [];
+var OneSignal = window.OneSignal || []
+if (!window.OneSignal_Initialized) {
   OneSignal.push(function() {
     OneSignal.init({
-      appId: "8e067b5e-0309-4726-b855-d7c0aac698d7",
-    });
-  });
+      appId: '8e067b5e-0309-4726-b855-d7c0aac698d7',
+    })
+    window.OneSignal_Initialized = true
+  })
+}
