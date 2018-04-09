e71ac1386d3ad82bfc965317782a8370b5bd264c
Kristian Freeman <kristian@kristianfreeman.com>
2018-04-09 07:48:17 -0700

    04/09/18

--- src/data.js
+++ src/data.js
@@ -20,7 +20,7 @@ const data = {
   question: {
     emoji: ':female-cook:',
     text: "What's the best open-source project you've discovered this month?",
-    url: 'https://twitter.com/byteconf/status/983121307188412421',
+    url: 'https://twitter.com/byteconf/status/983355847727308800',
   },
 }
 
